802be635cad1d717a0fbfcb7ec0cde0b4287842d
adam <napso20@gmail.com>
2023-07-20 16:15:22 +0300

    converting double quotes to single quotes

--- tests/unittests/test_bank_account_validation.py
+++ tests/unittests/test_bank_account_validation.py
@@ -5,21 +5,21 @@ from israel_bank_account_validator import validate_bank_account
 class TestBankAccountValidation(unittest.TestCase):
 
     def test_valid_account(self):
-        bank_code = "11"  # BANK DISCOUNT
-        branch_code = "111"
-        account_number = "000032018"
+        bank_code = '11'  # BANK DISCOUNT
+        branch_code = '111'
+        account_number = '000032018'
         self.assertTrue(validate_bank_account(bank_code, branch_code, account_number))
 
     def test_invalid_account(self):
-        bank_code = "20"  # BANK MIZRAHI TEFAHOT
-        branch_code = "006"
-        account_number = "000160779"
+        bank_code = '20'  # BANK MIZRAHI TEFAHOT
+        branch_code = '006'
+        account_number = '000160779'
         self.assertFalse(validate_bank_account(bank_code, branch_code, account_number))
 
     def test_empty_values(self):
-        bank_code = ""
-        branch_code = ""
-        account_number = ""
+        bank_code = ''
+        branch_code = ''
+        account_number = ''
         self.assertFalse(validate_bank_account(bank_code, branch_code, account_number))
 
     def test_non_string_input(self):
@@ -29,15 +29,15 @@ class TestBankAccountValidation(unittest.TestCase):
         self.assertFalse(validate_bank_account(bank_code, branch_code, account_number))
 
     def test_unsupported_bank(self):
-        bank_code = "99999999999"
-        branch_code = "006"
-        account_number = "000160779"
+        bank_code = '99999999999'
+        branch_code = '006'
+        account_number = '000160779'
         self.assertFalse(validate_bank_account(bank_code, branch_code, account_number))
 
     def test_negative_values(self):
-        bank_code = "11"
-        branch_code = "-6"
-        account_number = "000160779"
+        bank_code = '11'
+        branch_code = '-6'
+        account_number = '000160779'
         self.assertFalse(validate_bank_account(bank_code, branch_code, account_number))
 
 
